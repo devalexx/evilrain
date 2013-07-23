5a924132233b64e83ecaf58301abf7d17b666597
ashubenkov <devalexx@gmail.com>
2013-07-23 12:59:25 +0400

    remove wrong license

--- Main/src/com/alex/rain/helpers/Box2DSeparatorHelper.java
+++ Main/src/com/alex/rain/helpers/Box2DSeparatorHelper.java
@@ -1,31 +1,3 @@
-/*
- *   Copyright(c) 2001-2012, Latista Technologies Inc, All Rights Reserved.
- *
- *   The software and information contained herein are copyrighted and
- *   proprietary to Latista Technologies Inc. This software is furnished
- *   pursuant to a written license agreement and may be used, copied,
- *   transmitted, and stored only in accordance with the terms of such
- *   license and with the inclusion of the above copyright notice. Please
- *   refer to the file "LICENSE" for further copyright and licensing
- *   information. This software and information or any other copies
- *   thereof may not be provided or otherwise made available to any other
- *   person.
- *
- *   LATISTA TECHNOLOGIES INC MAKES NO REPRESENTATIONS AND EXTENDS NO
- *   WARRANTIES, EXPRESS OR IMPLIED, WITH RESPECT TO THE SOFTWARE, INCLUDING,
- *   BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *   FOR ANY PARTICULAR PURPOSE, AND THE WARRANTY AGAINST INFRINGEMENT OF
- *   PATENTS OR OTHER INTELLECTUAL PROPERTY RIGHTS. THE SOFTWARE IS PROVIDED
- *   "AS IS", AND IN NO EVENT SHALL LATISTA TECHNOLOGIES INC OR ANY OF ITS
- *   AFFILIATES BE LIABLE FOR ANY DAMAGES, INCLUDING ANY LOST PROFITS OR OTHER
- *   INCIDENTAL OR CONSEQUENTIAL DAMAGES RELATING TO THE SOFTWARE.
- *
- *   Please note that this software and information are protected by copyright
- *   law and international treaties. Unauthorized use, copy and/or modification
- *   of this software and information, may result in severe civil and criminal
- *   penalties, and will be prosecuted to the maximum extent possible under the
- *   law.
- */
 package com.alex.rain.helpers;
 
 import com.badlogic.gdx.math.Vector2;
