081702060b9e09a0e989cb30ab3b856141d0a3ab
ashubenkov <devalexx@gmail.com>
2015-03-03 15:32:10 +0300

    made first level harder

--- Android/assets/data/levels/level1.lua
+++ Android/assets/data/levels/level1.lua
@@ -3,24 +3,24 @@ dynActorArray = {}
 function onCreate(world)
     world:setWinHint('Drop houses!')
 
-    ground = luajava.new(Ground)
+    local ground = luajava.new(Ground)
     ground:addVertex(752, 48)
     ground:addVertex(752, 344)
     ground:addVertex(696, 208)
     ground:addVertex(600, 208)
     ground:addVertex(504, 136)
     ground:addVertex(96, 136)
-    ground:addVertex(32, 360)
+    ground:addVertex(32, 40)
     ground:addVertex(32, 32)
     world:add(ground)
 
-    dynamicActor = luajava.new(Home)
+    local dynamicActor = luajava.new(Home)
     world:add(dynamicActor)
     dynamicActor:setPosition(luajava.new(Vector2, 650, 300))
     table.insert(dynActorArray, dynamicActor)
 
     for i = 0, 3 do
-        dynamicActorTmp = luajava.new(Home)
+        local dynamicActorTmp = luajava.new(Home)
         dynamicActorTmp:setBodyBox(20, 100 + i * 10)
         dynamicActorTmp:setSpriteBox(20, 100 + i * 10)
         world:add(dynamicActorTmp)
@@ -29,14 +29,15 @@ function onCreate(world)
         table.insert(dynActorArray, dynamicActorTmp)
     end
 
-    cloud = luajava.new(Cloud)
-    cloud:setPosition(luajava.new(Vector2, 650, 420))
+    local cloud = luajava.new(Cloud)
+    cloud:setPosition(luajava.new(Vector2, 50, 420))
     world:add(cloud)
 end
 
 function onCheck()
     for i = 1, #dynActorArray do
-        if dynActorArray[i]:getRotation() > -30 and dynActorArray[i]:getRotation() < 30 then
+        if dynActorArray[i]:getRotation() > -30 and dynActorArray[i]:getRotation() < 30 and
+                dynActorArray[i]:getPosition().y > 0 then
             return false
         end
     end
